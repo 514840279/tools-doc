6091bae7364af44dd9c818944a163871bf685811
王滕辉 <514840279@qq.com>
2022-06-22 23:29:27 +0800

    1

--- docs/h2/start-allcluster.bat
+++
@@ -1,3 +0,0 @@
-start start-clusterserver1.bat
-start start-clusterserver2.bat
-start start-mastercontrol.bat
--- docs/h2/start-clusterserver1.bat
+++
@@ -1,3 +0,0 @@
-
-java -cp h2-1.4.200.jar org.h2.tools.Server -baseDir ./data/data1/ -ifNotExists -tcp -tcpAllowOthers -tcpPort 9091  -web -webAllowOthers  -webPort 9501 -tcpShutdownForce -browser
-pause
